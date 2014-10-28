02a5bccab76fa3ae75020bd378213f0a7401e7c2
Zoki <zoki@somedomain.com>
2014-10-28 20:18:22 +0100

    The 'server.js' file moved to the 'tests' folder.

---
+++ tests/server.js
@@ -0,0 +1,8 @@
+var http = require("http"),
+    connect = require("connect"),
+    app = connect(),
+    serveStatic = require("serve-static");
+    
+app.use(serveStatic(__dirname + "/../public"));
+
+http.createServer(app).listen(4000);
